c8499b78dcbf23c294defae92ef6957431a41bd0
Behnam Asadi <behnam.asadi@gmail.com>
2023-01-10 10:30:23 +0100

    Update euler_angle.cpp

--- src/geometry_transformation/euler_angle.cpp
+++ src/geometry_transformation/euler_angle.cpp
@@ -11,21 +11,33 @@ Eigen::Matrix3d eulerAnglesToRotationMatrix(double roll, double pitch,
   return rotationMatrix;
 }
 
+Eigen::Vector3d rotationMatrixToEulerAngles(Eigen::Matrix3d rotationMatrix)
+{
+    Eigen::Vector3d euler_angles = rotationMatrix.eulerAngles(2, 1, 0);
+    return euler_angles;
+}
+
 int main()
 {
 
-
     double roll, pitch, yaw;
 
-    roll = M_PI / 4;
-    pitch = M_PI / 3;
-    yaw = M_PI / 6;
-
-    Eigen::Matrix3d rotationMatrix =
-        eulerAnglesToRotationMatrix(roll, pitch, yaw);
+    roll = M_PI / 8;
+    pitch = M_PI / 4;
+    yaw = M_PI / 9;
 
 
-    // Rotation Matrix to Euler Angle (Proper)
-    Eigen::Vector3d euler_angles = rotationMatrix.eulerAngles(2, 0, 2);
+    std::cout << "set angles:" << std::endl;
+
+    std::cout << "roll: Pi/" << M_PI / roll << " ,pitch: Pi/" << M_PI / pitch << " ,yaw : Pi/" << M_PI / yaw
+              << std::endl;
+
+    Eigen::Matrix3d rotationMatrix = eulerAnglesToRotationMatrix(roll, pitch, yaw);
+    Eigen::Vector3d euler_angles = rotationMatrixToEulerAngles(rotationMatrix);
+
+    std::cout << "retrieved angles:" << std::endl;
+
+    std::cout << "roll: Pi/" << M_PI / euler_angles[2] << " ,pitch: Pi/" << M_PI / euler_angles[1] << " ,yaw : Pi/"
+              << M_PI / euler_angles[0] << std::endl;
 
 }
